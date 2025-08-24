d3567e5c85d8f399a9e970a143a49f2d1460845d
Alison G. Martins <odevalison@gmail.com>
2025-08-24 00:35:02 -0300

    chore: organize application

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,12 @@
+import { Container } from "@/components/container";
 import { Hero } from "@/components/hero";
+import { ContactsFooter } from "@/components/home/contacts-footer";
 import { Services } from "@/components/home/services";
 import { Submenu } from "@/components/home/submenu";
 import { getHomeData } from "@/utils/actions/get-home-data";
 import type { HomeAPIResponseData } from "@/utils/home.type";
 import { PhoneIcon } from "lucide-react";
 import styles from "./styles.module.scss";
-import { Container } from "@/components/container";
-import { ContactsFooter } from "@/components/home/contacts-footer";
 
 const HomePage = async () => {
   const { object }: HomeAPIResponseData = await getHomeData();
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
+import { Header } from "@/components/header";
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.scss";
-import { Header } from "@/components/header";
 
 const sora = Sora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DevMotors - Sua oficina especializada",
+  title: "DevMotors - Oficina Dev especializada!",
   description:
-    "Oficina de carros DevMotors em São Paulo, desde 2011 especializada em carros de Desenvolvedores Web!",
+    "Oficina Automotiva DevMotors em São Paulo, desde 2011 especializada em consertar carros de Desenvolvedores Web!",
 };
 
 export default function RootLayout({
